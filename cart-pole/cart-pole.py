354b412c16e319a6e40208455e2c03015d66d62e
Matthew Soh <matthewsoh.cw@gmail.com>
2017-02-16 13:24:51 -0800

    Removed multidim

--- cart-pole/cart-pole.py
+++ cart-pole/cart-pole.py
@@ -11,8 +11,11 @@ from keras.layers import Dense
 env = gym.make('CartPole-v0')
 env = wrappers.Monitor(env, "/tmp/cartpole0", force = True)
 
+"""
+
 def multidim(dim, init = lambda: None):
     return np.random.rand(*dim)
+"""
 
 def multidex(iterable, index):
     return iterable[index]
@@ -31,7 +34,7 @@ class TabularLearner:
     TIMESTEP_MAX = 200
 
     def __init__(self, actions, buckets = BUCKETS):
-        self.Q = multidim(buckets + (actions,), lambda: random.random())
+        self.Q = np.random.rand(*buckets, actions)
         self.eps = 0.3
         self.alpha = 1
         self.actions = actions
@@ -46,8 +49,17 @@ class TabularLearner:
         self.eps = max(0, self.eps)
         self.alpha -= 0.0001
         return action
+    
+    def learn(*args):
+        pass
 
-    def learn(self, observation, reward, done, t):
+    def discretized(self, observation):
+        eps = 0.000001
+        bounded = [min(self.LIMITS[i] - eps,max(-self.LIMITS[i] + eps, observation[i])) for i in range(len(observation))]
+        return tuple(math.floor((bounded[i] + self.LIMITS[i]) / 2 / self.LIMITS[i] * self.BUCKETS[i]) for i in range(len(bounded)))
+        
+class TabularQLearner(TabularLearner):
+    def learn(self, s0, observation, reward, action, done, t):
         a_t, s_t = self.last
         prev = multidex(self.Q, self.discretized(s_t) + (a_t,))
         now = multidex(self.Q, self.discretized(observation))
@@ -58,14 +70,6 @@ class TabularLearner:
             else:
                 update = 100
         set_multidex(self.Q, self.discretized(s_t) + (a_t,), update)
-
-    def discretized(self, observation):
-        eps = 0.000001
-        bounded = [min(self.LIMITS[i] - eps,max(-self.LIMITS[i] + eps, observation[i])) for i in range(len(observation))]
-        return tuple(math.floor((bounded[i] + self.LIMITS[i]) / 2 / self.LIMITS[i] * self.BUCKETS[i]) for i in range(len(bounded)))
-        
-class TabularQLearner(TabularLearner):
-    pass
 
 class TabularSARSALearner(TabularLearner):
     def learn(self, observation, reward, done):
@@ -158,7 +162,8 @@ max_steps = env.spec.tags.get('wrapper_config.TimeLimit.max_episode_steps')
 
 def main():
     total = 0
-    learner = DeepQLearner(len(env.observation_space.high), (8, 16, 32, env.action_space.n), 128, 10000)
+    #learner = DeepQLearner(len(env.observation_space.high), (8, 16, 32, env.action_space.n), 128, 10000)
+    learner = TabularQLearner(env.action_space.n)
     for i_episode in range(1000):
         observation = env.reset()
         ep_reward = 0
